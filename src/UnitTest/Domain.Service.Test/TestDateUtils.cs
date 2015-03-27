c0b7bfb44768e7c3e23ff462ac62a97ac42de2d7
qunxi <wangqunxi@gmail.com>
2015-03-27 16:40:00 +0800

    modify unit test

--- src/UnitTest/Domain.Service.Test/TestDateUtils.cs
+++ src/UnitTest/Domain.Service.Test/TestDateUtils.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Application.Test
 {
-    using Doamin.Service.Factory;
+    using Infrastructure.Utility;
 
     [TestClass]
     public class TestDateUtils
@@ -14,7 +14,7 @@ namespace Application.Test
             DateTime now = new DateTime(2015, 3, 22);
             DateTime startTime = new DateTime(2015, 3, 16);
             DateTime endTime = new DateTime(2015, 3, 22);
-            Tuple<DateTime, DateTime> range = DateUtils.GetWeekRangeOfCurrentDate(now);
+            Tuple<DateTime, DateTime> range = DateHelper.GetWeekRangeOfCurrentDate(now);
             Assert.IsTrue(startTime.Equals(range.Item1));
             Assert.IsTrue(endTime.Equals(range.Item2));
         }
