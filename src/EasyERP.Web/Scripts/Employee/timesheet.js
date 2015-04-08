9f3440e21d7d8e4882734d185f9448bbba2c9a57
qunxi <wangqunxi@gmail.com>
2015-04-08 23:09:02 +0800

    add timesheet

--- src/EasyERP.Web/Scripts/Employee/timesheet.js
+++ src/EasyERP.Web/Scripts/Employee/timesheet.js
@@ -1,36 +1,83 @@
 ﻿$(document).ready(function () {
-    $("#datepicker").kendoDatePicker({
-        format: "yyyy-MM-dd"
-    });
-
+   
     var dataSource = new kendo.data.DataSource({
         transport: {
             read: {
-                url: "index",
-                dataType: "jsonp"
+                //url: "employee/employeeList",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
             },
-            update: {
-                url: "update",
-                dateType: "jsonp"
+            schema: {
+                data: "data",
+                total: "total"
+            }
+        },
+        serverPaging: true,
+        pageSize: 2,
+        schema: {
+            data: "data",
+            total: "total",
+            model: {
+                fields: {
+                    Id: { editable: false, nullable: true },
+                    FullName: { type: "string" },
+                    Sex: { type: "string" },
+                    IdNumber: { type: "string" },
+                    Address: { type: "string" },
+                    CellPhone: { type: "string" },
+                    Education: { type: "string" },
+                    NativePlace: { type: "string" }
+                }
             }
         }
     });
 
-    $("#timesheet").kendoGrid({
-        dataSource: dataSource,
-        height: 500,
-        pageable: {
-            refresh: true,
-            pageSizes: true,
-            buttonCount: 5
-        },
-        columns: [
-        {
-            title: "a",
-            field: "d"
-        }, {
-            title: "c",
-            field: "d"
-        }]
+    function generateColumns(selDay) {
+        function getSelectedWeek() {
+            var columns = [{field: "EmployeeName", title: "姓名"}];
+            var weeks = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"];
+            var fields = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+            console.log(selDay);
+            var firstDayOfWeek = selDay.getDate() - (selDay.getDay() + 7 - 1) % 7;
+            for (var i = 0; i < 7; ++i) {
+                //console.log(firstDayOfWeek);
+                var tempDate = new Date(selDay);
+                tempDate.setDate((firstDayOfWeek + i));
+                columns.push({ field: fields[i], title: (weeks[i] + "(" + tempDate.toLocaleDateString()) + ")" });
+            }
+            return columns;
+        }
+
+        return getSelectedWeek(selDay);
+    }
+
+    var CurrentDate = new Date();
+
+    initialKendoGrid();
+
+    function initialKendoGrid() {
+        $("#timesheet").kendoGrid({
+            dataSource: dataSource,
+            height: 400,
+            selectable: "multiple",
+            resizable: true,
+            pageable: {
+                refresh: true,
+            },
+            columns: generateColumns(CurrentDate)
+        });
+    }
+
+    $("#preWeek").click(function() {
+        CurrentDate.setDate(CurrentDate.getDate() - 7);
+        $("#timesheet").empty();
+        initialKendoGrid();
     });
+
+    $("#nextWeek").click(function() {
+        CurrentDate.setDate(CurrentDate.getDate() + 7);
+        $("#timesheet").empty();
+        initialKendoGrid();
+    });
+
 });
